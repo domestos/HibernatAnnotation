eaf49983da062521caf0d4229a22885d15b3ea7b
unknown <v.pelenskyi@VWS04.dzk.lan>
2015-07-13 17:18:56 +0300

    Not use constructor add new Select

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,9 @@ import com.company.dao.imp.CartrigeDaoImpl;
 import com.company.dao.imp.CompanyDaoImpl;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Main {
 
@@ -17,7 +19,7 @@ public class Main {
         CompanyDaoImpl companyDaoImpl= new CompanyDaoImpl();
         CartrigeDaoImpl cartrigeDaoImpl = new CartrigeDaoImpl();
 
-
+/*
         List<Cartridge> cartridgeList = null;
         Company company = new Company();
 
@@ -33,35 +35,52 @@ public class Main {
         cartridge2.setNumberLink("1659");
         cartridge2.setRqLink("RQLink");
 
-
-
-
         company.setCartridges(cartridgeList);
         company.setNameCompany("DZK Company");
         company.setMobCumber("0676724468");
 
-        //ADD
-        //-----------------------------------------------------------
-        companyDaoImpl.addDataBase(company);
+        //ADD Company and create List<Cartridge>
         //-----------------------------------------------------------
         cartrigeDaoImpl.addDataBase(cartridge);
         cartrigeDaoImpl.addDataBase(cartridge2);
+        // companyDaoImpl.addDataBase(company);
+        //-----------------------------------------------------------
         //GET ALL
         //-----------------------------------------------------------
-
         List<Company> companies = companyDaoImpl.getAllDataBase();
         System.out.println("Get all company: " );
-        for (Company com: companies){
-            System.out.println(com.getId()+" "+
-                    com.getMobCumber()+" "+
-                    com.getNameCompany()+" ");
-        }
+        companyDaoImpl.printListCompany(companies);
         //-------------------------------------------------------------
 
+        //GET ALL SQLQuvery
+        //-------------------------------------------------------------
+        List<Company> companiesSQLQuvery =  companyDaoImpl.getSqlQueryAllCompany();
+        System.out.println("Get companiesSQLQuvery All : " );
+        companyDaoImpl.printListCompany(companiesSQLQuvery);
+        //-------------------------------------------------------------
 
+        //GET ID SQLQvery
+        //-------------------------------------------------------------
+        List<Company> companiesSQLQuveryID = companyDaoImpl.getSQLQuveryID(13);
+        System.out.println("Get SQLQuvery ID: " );
+        companyDaoImpl.printListCompany(companiesSQLQuveryID);
+        //-------------------------------------------------------------
+
+        //GET ID HQL
+        //-------------------------------------------------------------
+        System.out.println("Get ID HQL  " );
+        List<Company> companyHQLID = companyDaoImpl.getHQLID(19);
+        //-------------------------------------------------------------
+ */       //GET OneToMany
+        //-------------------------------------------------------------
+        System.out.println("OneToMany HQL");
+        companyDaoImpl.getOneToMany();
+
+        //-------------------------------------------------------------
+/*
         //GET ID
         //-----------------------------------------------------------
-        Company returnGetId =  companyDaoImpl.getIdDataBase(9);
+        Company returnGetId =  companyDaoImpl.getIdDataBase(12);
         System.out.println("Get id company: " );
         System.out.println(returnGetId.getId()+" "+
                 returnGetId.getMobCumber()+" "+
@@ -69,11 +88,27 @@ public class Main {
 
         //-----------------------------------------------------------
 
+
         //DELETE
         //-----------------------------------------------------------
         companyDaoImpl.deleteDataBase(returnGetId);
-        //-----------------------------------------------------------
 
+
+        if(!companyHQLID.isEmpty() && companyHQLID.size() == 1){
+            System.out.println("You delete this object");
+            Company comp = companyHQLID.get(0);
+            System.out.println(comp.getId()+" = id " +
+                    comp.getNameCompany()+" = name ");
+            companyDaoImpl.deleteDataBase(comp);
+        }else{
+            System.out.println("this object empty");
+        }
+        //-----------------------------------------------------------
+*/
 
     }
+
+
+
+
 }
--- src/com/company/dao/imp/CompanyDaoImpl.java
+++ src/com/company/dao/imp/CompanyDaoImpl.java
@@ -4,11 +4,13 @@ import com.company.Company;
 import com.company.DataBase;
 import com.company.dao.HibernateDao;
 import com.company.util.HibernatUtil;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
+import org.hibernate.*;
+import org.hibernate.criterion.Restrictions;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Objects;
+import java.util.Queue;
 
 /**
  * Created by v.pelenskyi on 08.07.2015.
@@ -84,11 +86,84 @@ public class CompanyDaoImpl implements HibernateDao {
         return company;
     }
 
-    public void testMethod(){
-
+    //вивести усі компанії через sql запит
+    public List<Company> getSqlQueryAllCompany() throws SQLException {
+        List<Company> resultList  = null;
+        Session session = null;
+        try{
+            session = HibernatUtil.getSessionFactory().openSession();
+            SQLQuery query = session.createSQLQuery("SELECT * FROM Company").addEntity(Company.class);
+            resultList = query.list();
+            return resultList ;
+        }finally {
+            closeSession(session);
+        }
     }
 
-    private void closeSession(Session session) {
+    // вивести компанію через задане ід
+    public List<Company> getSQLQuveryID(int paramID)throws SQLException {
+        List<Company> result = null;
+        Session session = null;
+
+        try{
+            session=HibernatUtil.getSessionFactory().openSession();
+            SQLQuery quvery = (SQLQuery) session.createSQLQuery("SELECT * FROM Company c WHERE c.id = :paramID").addEntity(Company.class).setParameter("paramID", paramID);
+            result= quvery.list();
+        }finally {
+            closeSession(session);
+        }
+
+        return  result;
+    }
+
+    // вивести компанію через задане ід HQL
+    public List<Company> getHQLID(int paramID)throws SQLException {
+        List<Company> result = null;
+        Session session = null;
+
+        try{
+            session=HibernatUtil.getSessionFactory().openSession();
+            Criteria cr = session.createCriteria(Company.class);
+            cr.add(Restrictions.eq("id", paramID));
+            result = cr.list();
+            }finally {
+            closeSession(session);
+        }
+        return  result;
+    }
+    //GET OneToMany
+    public void getOneToMany()throws SQLException  {
+        Session session = HibernatUtil.getSessionFactory().openSession();
+      try{
+
+          SQLQuery query = session.createSQLQuery("SELECT name_company, mob_number, car.model_cartridge, car.number_link, car.rq_link" +
+                  " FROM Company  comp " +
+                  "INNER JOIN Cartridge car ON car.id_company = comp.id ");
+
+        List<DataBase []> resultObj = query.list();
+        for (Object [] obj: resultObj){
+            for(Object ob: obj){
+                System.out.print(ob + " ");
+            }
+            System.out.println();
+        }
+      }finally {
+          closeSession(session);
+      }
+    }
+
+
+    public void printListCompany(List<Company> companyList){
+        for (Company com: companyList){
+            System.out.println(com.getId()+" "+
+                    com.getMobCumber()+" "+
+                    com.getNameCompany()+" ");
+        }
+    }
+
+
+
+    private void closeSession(Session session) throws SQLException {
         if ((session != null) && (session.isOpen())) {
             session.close();
         }
